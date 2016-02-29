dda76128fd61268ca9ada36fd63be9241955e444
Yeti Sno <yeti@yetiz.org>
2016-02-29 22:23:21 +0800

    add some file

---
+++ mds-utils/src/main/java/org/yetiz/utils/mds/Service.java
@@ -0,0 +1,8 @@
+package org.yetiz.utils.mds;
+
+/**
+ * mds
+ * Created by yeti on 16/2/29.
+ */
+public interface Service {
+}
---
+++ service-entry/src/main/java/org/yetiz/serv/mds/service_entry/ServiceEntry.java
@@ -0,0 +1,10 @@
+package org.yetiz.serv.mds.service_entry;
+
+import org.yetiz.utils.mds.Service;
+
+/**
+ * mds
+ * Created by yeti on 16/2/29.
+ */
+public class ServiceEntry implements Service {
+}
