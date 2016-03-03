9da566714a119f5d85565f9af381e003f1def03e
Yeti Sno <yeti@yetiz.org>
2016-03-03 23:45:08 +0800

    add func to Service

--- mds-utils/src/main/java/org/yetiz/utils/mds/Service.java
+++ mds-utils/src/main/java/org/yetiz/utils/mds/Service.java
@@ -1,8 +1,19 @@
 package org.yetiz.utils.mds;
 
+import java.util.Map;
+
 /**
  * mds
  * Created by yeti on 16/2/29.
  */
 public interface Service {
+    boolean start(Object... params);
+
+    boolean stop(Object... params);
+
+    boolean reload(Object... params);
+
+    boolean restart(Object... params);
+
+    Map<String, Object> status(Object... params);
 }
