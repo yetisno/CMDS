ac2ed43880e180f76ab108996be8468408864aa9
Yeti Sno <yeti@yetiz.org>
2016-03-06 01:03:39 +0800

    rename

--- cmds-utils/src/main/java/org/yetiz/utils/cmds/utils/Run.java
+++ cmds-utils/src/main/java/org/yetiz/utils/cmds/utils/Run.java
@@ -4,7 +4,7 @@ package org.yetiz.utils.cmds.utils;
  * cmds
  * Created by yeti on 16/3/6.
  */
-public class Rund {
+public class Run {
 
     public static void withoutException(Runnable runnable) {
         try {
