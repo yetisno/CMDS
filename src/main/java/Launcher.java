59fa21e5010c2a9a62a25be6d821c7ed708c3e91
Yeti Sno <yeti@yetiz.org>
2016-03-29 22:04:26 +0800

    udpate Launcher

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,9 +1,15 @@
+import org.apache.logging.log4j.core.config.yaml.YamlConfigurationFactory;
+
 /**
  * cmds
  * Created by yeti on 16/2/29.
  */
 public class Launcher {
 
-    public static void main(String... args) {
-    }
+	static {
+		System.setProperty(YamlConfigurationFactory.CONFIGURATION_FILE_PROPERTY, "logger.yaml");
+	}
+
+	public static void main(String... args) {
+	}
 }
